ab39fee32eb919ab06a983c644a7952a1734b81c
Ali Toghani <at007@uw.edu>
2024-02-25 16:41:16 -0800

    Updating def

--- project/main.py
+++ project/main.py
@@ -21,16 +21,28 @@ if __name__ == "__main__":
     cutoff = df.find_min_length(data_with_frame)
     end = df.find_max_length(data_with_frame)
 
+    full_list = []
+    for track in data_with_frame:
+            new_track = df.listTrim(track, cutoff)
+            full_list = dm.list_calculate(new_track)
     final_result = []
     while (cutoff <= end):
+
         temp_res = []
+        print("started on " + str(cutoff))
+
         for track in data_with_frame:
+
             if len(track) < cutoff:
                 continue
+
             new_track = df.listTrim(track, cutoff)
             temp = dm.list_calculate(new_track)
             temp_res.append(temp)
 
         final_result.append(df.calculate_avg(temp_res))
+        cutoff = cutoff + 1
+
     print(final_result)
+    print(df.calculate_avg)
     #data_manipulation.list_calculate([1,2,3 ], [1,2 , 3], [1,2, 3])
